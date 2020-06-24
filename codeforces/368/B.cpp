7c9e0fff263298547c746576fbc05f9f4bf1e014
Nazmus Sakib Ahmed <33sakib33@gmail.com>
2020-06-24 22:39:00 +0600

    Add solution for problem `B - Sereja and Suffixes` Link: https://codeforces.com/contest/368/problem/B Tags: data structures, dp, *1100 Ref: https://codeforces.com/contest/368/submission/84909775

---
+++ codeforces/368/B.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define set0(a) memset ((a), 0 , sizeof(a))
+#define set1(a) memset((a),-1,sizeof (a))
+#define pi pair<int, int>
+#define ps pair<string, string>
+#define pl pair<long, long>
+#define pll pair<long long, long long>
+#define vll vector<long long>
+#define vl vector<long>
+#define vi vector<int>
+#define vs vector<string>
+#define vps vector< ps >
+#define vpi vector< pi >
+#define vpl vector< pl >
+#define vpll vector< pll >
+#define flash  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tc(t,T) for(long long t=0;t<T;t++)
+#define rep(i,s,n,d) for(long long i=s;i<n;i=i+d)
+
+bool sortbysec(const pll &a,
+              const pll &b)
+{
+    return (a.second < b.second);
+}
+/*bool cxp pair<ll, pll> &a,
+              const pair<ll, pll> &b)
+{
+    return (a.ff < b.ff);
+}*/
+
+void func(void)
+{
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+}
+ll gcd(ll a,ll b){
+    if(b==0)return a;
+    else return gcd(b,a%b);
+}
+int main(){
+  ll n,m;
+  cin>>n>>m;
+  vll a(n);
+  rep(i,0,n,1)cin>>a[i];
+  vll b(n,0);
+  ll count1=0;
+ set<ll> d;
+  for(ll i=n-1;i>=0;i--){
+    d.insert(a[i]);
+    b[i]=d.size();
+  }
+    rep(i,0,m,1){
+        ll temp;
+        cin>>temp;
+        cout<<b[temp-1]<<endl;
+    }
+}
+
+
+// 1 2 3 1 5 3 1 0 1 0 2
+// 1 2 3 4 5 6 7 8 9 10 11
