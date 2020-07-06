0a02ad3778dd37af7ceaa86396191c0f955a5e83
Nazmus Sakib Ahmed <33sakib33@gmail.com>
2020-07-06 23:10:00 +0600

    Add solution for problem `C - Element Extermination` Link: https://codeforces.com/contest/1375/problem/C Tags: constructive algorithms, data structures, greedy, *1400 Ref: https://codeforces.com/contest/1375/submission/86145233

---
+++ codeforces/1375/C.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    vector<long long> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    if(a[0]<a[n-1])cout<<"YES\n";
+    else cout<<"NO\n";
+    }
+}
