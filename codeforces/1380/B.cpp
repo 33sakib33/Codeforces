831652e8cdd1413c44e03551ed84fde5d8cdd4aa
Nazmus Sakib Ahmed <33sakib33@gmail.com>
2020-07-12 21:55:00 +0600

    Add solution for problem `B - Universal Solution` Link: https://codeforces.com/contest/1380/problem/B Tags: greedy, *1400 Ref: https://codeforces.com/contest/1380/submission/86680720

---
+++ codeforces/1380/B.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000009
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define set0(a) memset ((a), 0 , sizeof(a))
+#define set1(a) memset((a),-1,sizeof (a))
+#define pi pair<int, int>
+#define ps pair<string, string>
+#define pl pair<long, long>
+#define pll pair<long long, long long>
+#define vll vector<long long>
+#define vl vector<long>
+#define vi vector<int>
+#define vs vector<string>
+#define vps vector< ps >
+#define vpi vector< pi >
+#define vpl vector< pl >
+#define vpll vector< pll >
+#define flash  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tc(t,T) for(long long t=0;t<T;t++)
+#define rep(i,s,n,d) for(long long i=s;i<n;i=i+d)
+
+bool sortbysec(const pll &a,
+              const pll &b)
+{
+    return (a.second < b.second);
+}
+/*bool cxp pair<ll, pll> &a,
+              const pair<ll, pll> &b)
+{
+    return (a.ff < b.ff);
+}*/
+
+void func(void)
+{
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+}
+ll gcd(ll a,ll b){
+    if(b==0)return a;
+    else return gcd(b,a%b);
+}
+void printStack(stack<string> s){
+    if(s.empty())return;
+
+    string x=s.top();
+    s.pop();
+    printStack(s);
+    cout<<x;
+    s.push(x);
+
+}
+
+
+ll binaryexp(ll n,ll p){
+
+    if(p==0) return 1;
+    if(p%2==0) return binaryexp((n%mod)*(n%mod),p/2);
+    else return ((n%mod)*binaryexp(((n%mod)*(n%mod))%mod,(p-1)/2)%mod);
+
+}
+
+#define meax 10000001
+vll spf(meax,0);
+void siv(){
+    spf[1]=1;
+
+   for(ll i=2;i<meax;i++)spf[i]=i;
+
+   for(ll i=4;i<meax;i+=2){
+        spf[i]=2;
+    }
+    for(ll i=3;i*i<meax;i++){
+        if(spf[i]==i){
+            for(ll j=i*i;j<meax;j+=i){
+                if(spf[j]==j){
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+}
+
+int main(){
+        ll T;
+        cin>>T;
+        tc(t,T){
+            string s;
+            cin>>s;
+            char z;
+            int i=0;
+
+            int a[3]={};
+            ll x=0;
+            for(i=0;i<s.size();i++){
+                if(s[i]=='R')x=0;
+                else if(s[i]=='S')x=1;
+                else x=2;
+                a[x]++;
+            }
+            ll max1=0;
+            ll z1=0;
+            for(int j=0;j<3;j++){
+                if(max1<a[j]){
+                   z1=j;
+                   max1=a[j];
+                }
+            }
+            if(z1==0)z='P';
+            else if(z1==1)z='R';
+            else z='S';
+            for(i=0;i<s.size();i++)cout<<z;
+            cout<<"\n";
+        }
+}
+
