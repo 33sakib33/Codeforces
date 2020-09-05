91c6dc5dfae86a2ff401c69f2fbc32ae6de29e0a
Nazmus Sakib Ahmed <33sakib33@gmail.com>
2020-09-06 04:35:00 +0600

    Add solution for problem `C - Cow and Message` Link: https://codeforces.com/contest/1307/problem/C Tags: brute force, dp, math, strings, *1500 Ref: https://codeforces.com/contest/1307/submission/91973746

---
+++ codeforces/1307/C.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000009
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define nl printf("\n")
+#define ff first
+#define ss second
+#define set0(a) memset ((a), 0 , sizeof(a))
+#define set1(a) memset((a),-1,sizeof (a))
+#define pi pair<int, int>
+#define ps pair<string, string>
+#define pl pair<long, long>
+#define pll pair<long long, long long>
+#define vll vector<long long>
+#define vl vector<long>
+#define vi vector<int>
+#define vs vector<string>
+#define vps vector< ps >
+#define vpi vector< pi >
+#define vpl vector< pl >
+#define vpll vector< pll >
+#define flash  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tc(t,T) for(long long t=0;t<T;t++)
+#define rep(i,s,n,d) for(long long i=s;i<n;i=i+d)
+const int todx[] = { -2, -2, -1, -1,  1,  1,  2,  2 };
+const int tody[] = { -1,  1, -2,  2, -2,  2, -1,  1 };
+bool sortbysec(const pll &a,
+              const pll &b)
+{
+    return (a.second < b.second);
+}
+/*bool cxp pair<ll, pll> &a,
+              const pair<ll, pll> &b)
+{
+    return (a.ff < b.ff);
+}*/
+
+void func(void)
+{
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+}
+ll gcd(ll a,ll b){
+    if(b==0)return a;
+    else return gcd(b,a%b);
+}
+void printStack(stack<string> s){
+    if(s.empty())return;
+
+    string x=s.top();
+    s.pop();
+    printStack(s);
+    cout<<x;
+    s.push(x);
+
+}
+
+
+ll binaryexp(ll n,ll p){
+
+    if(p==0) return 1;
+    if(p%2==0) return binaryexp((n%mod)*(n%mod),p/2);
+    else return ((n%mod)*binaryexp(((n%mod)*(n%mod))%mod,(p-1)/2)%mod);
+
+}
+
+#define meax 10000001
+vll spf(meax,0);
+void siv(){
+    spf[1]=1;
+
+   for(ll i=2;i<meax;i++)spf[i]=i;
+
+   for(ll i=4;i<meax;i+=2){
+        spf[i]=2;
+    }
+    for(ll i=3;i*i<meax;i++){
+        if(spf[i]==i){
+            for(ll j=i*i;j<meax;j+=i){
+                if(spf[j]==j){
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+}
+
+
+int main(){
+    string s;
+    cin>>s;
+    map<char,ll> m;
+    for(char i='a';i<='z';i++){
+        m[i]=0;
+    }
+     map<string,ll> m2;
+    m[s[0]]++;
+    for(int i=1;i<s.size();i++){
+        for(char j='a';j<='z';j++){
+            ll d=m[j];
+            string s2(2,j);
+            s2[1]=s[i];
+            m2[s2]+=d;
+        }
+        m[s[i]]++;
+    }
+    ll max1=0;
+    for(auto x:m){
+        max1=max(x.ss,max1);
+    }
+    for(auto x:m2){
+        max1=max(x.ss,max1);
+    }
+    cout<<max1<<endl;
+
+}
+
