ced4940801406282e3e62d5e04590387b74fcee6
Nazmus Sakib Ahmed <33sakib33@gmail.com>
2020-09-16 05:47:00 +0600

    Add solution for problem `C - Phone Numbers` Link: https://codeforces.com/contest/940/problem/C Tags: constructive algorithms, implementation, strings, *1500 Ref: https://codeforces.com/contest/940/submission/92938134

--- codeforces/940/C.cpp
+++ codeforces/940/C.cpp
@@ -1,20 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,k,c=0,flag=0,arr[26];
-    pair<int,int>st[26];
-    string s,t;
-    cin>>n>>k>>s;
-    t.resize(k);
+    int k;
+    int n;
+    cin>>n;
+    cin>>k;
+    string s;
+    int st[26];
+    pair<char,int> yo[26];
+    int arr[26];
+    cin>>s;
     for(int i=0;i<s.size();i++){
-        st[s[i]-'a'].first=1;
+        st[s[i]-'a']=1;
     }
+    int prev=-1;
     for(int i=0;i<26;i++){
-        if(st[i].first==1){
-            st[i].second=c;
+        yo[0].first='a'+i;
+    }
+    int c=0;
+    for(int i=0;i<26;i++){
+        if(st[i]==1){
             arr[c++]=i;
         }
+        /*i=i%26;
+        if(st[yo[i].ff]==1){
+        yo[i].ss=prev;
+        prev=i;
+        }*/
+
     }
+
+    string t(k,'a');
+    int flag=0;
+    int flag1=0;
+
     for(int i=k-1;i>=0;i--){
             if(k>s.size()){
                 if(i>=s.size())
@@ -23,17 +42,29 @@ int main(){
 
             }
            else {
-                if(i==k-1){
-                    char x=s[i];
-                    t[i]='a'+arr[(st[x-'a'].second+1)%c];
-                    if(st[x-'a'].second+1>=c)flag=1;
-                }
+            for(int j=0;j<c;j++){
+                    int s1=s[i]-'\0';
+                    int s2=arr[j]+'a';
+
+                if(s1==s2){
+                    if(i==k-1 && flag==0 && flag1==0){
+                       t[i]='a'+arr[(j+1)%c];
+                         if(j+1>=c){
+                        flag=1;
+                    }
+                        flag1=1;
+                    }
                 else{
-                    char x=s[i];
-                    t[i]='a'+arr[(st[x-'a'].second+flag)%c];
-                    if(st[x-'a'].second+flag>=c)flag=1;
+
+                        t[i]= 'a'+arr[(j+flag)%c];
+                    if(j+flag>=c){
+                        flag=1;
+                    }
                     else flag=0;
                 }
+                }
+
+            }
            }
     }
     cout<<t<<endl;
